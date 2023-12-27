67b0fd2389b063d3f6b2bfe02153a2751a097475
olha.senchyk <olhasenchyk@gmail.com>
2023-12-27 16:24:58 +0100

    add-index

--- WebSocketExample/be/index.js
+++ WebSocketExample/be/index.js
@@ -1,29 +1,45 @@
-const WebSocket = require('ws');
+const { WebSocketServer, WebSocket } = require('ws');
+const http = require('http');
 const { v4: uuidv4 } = require('uuid');
 
-const wss = new WebSocket.Server({ port: 8080 });
-
+const server = http.createServer();
+const wsServer = new WebSocketServer({ server });
+const port = 8000;
 const clients = {};
+const users = {};
+const userActivity = [];
 
-wss.on('connection', function connection(ws) {
-  const userId = uuidv4();
-  clients[userId] = ws;
-
-  console.log(`New client connected: ${userId}`);
-
-  ws.on('message', function incoming(message) {
-    console.log(`Received message from ${userId}: ${message}`);
-    Object.values(clients).forEach(client => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(`User ${userId}: ${message}`);
-      }
-    });
-  });
-
-  ws.on('close', function() {
-    console.log(`Client disconnected: ${userId}`);
-    delete clients[userId];
-  });
+server.listen(port, () => {
+  console.log(`WebSocket server is running on port ${port}`);
 });
 
-console.log("WebSocket server is running on ws://localhost:8080");
+wsServer.on('connection', function(connection) {
+  const userId = uuidv4();
+  console.log(`Received a new connection.`);
+
+  clients[userId] = connection;
+  console.log(`${userId} connected.`);
+
+  connection.on('close', () => handleDisconnect(userId));
+});
+
+function broadcastMessage(json) {
+  const data = JSON.stringify(json);
+  for (let userId in clients) {
+    let client = clients[userId];
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  }
+}
+
+function handleDisconnect(userId) {
+  console.log(`${userId} disconnected.`);
+  const json = { type: "USER_EVENT" };
+  const username = users[userId]?.username || userId;
+  userActivity.push(`${username} left the document`);
+  json.data = { users, userActivity };
+  delete clients[userId];
+  delete users[userId];
+  broadcastMessage(json);
+}
